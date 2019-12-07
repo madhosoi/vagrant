1d3b169a475a0982cad14f2cb854db4e18201ef2
Miguel Angel Diego <madiego@sciops.esa.int>
2019-12-07 20:29:51 +0100

    Init upload

---
+++ rke/scripts/setup-pki.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+echo 'provision pki'
+
+ssh_user=vagrant
+home="$(eval echo ~$ssh_user)"
+
+echo "> setup pki ($ssh_user: $home)"
+
+curl -Ss https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub >> "$home/.ssh/authorized_keys"
+
+mkdir -p "$home/.ssh"
+
+if [ ! -e "$home/.ssh/id_rsa" ]; then
+  echo '> install insecure private ssh key'
+  curl -Ss https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant > "$home/.ssh/id_rsa"
+fi
+
+chown "$ssh_user" "$home/.ssh/id_rsa"
+chmod 400 "$home/.ssh/id_rsa"
